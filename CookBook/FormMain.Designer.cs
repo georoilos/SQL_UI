90537575b5e495af23a5f8cdae8578c7a3125b7d
George <georoilos@yahoo.com>
2017-08-08 12:42:50 +0100

    Added Sql Queries

---
+++ CookBook/FormMain.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CookBook
+{
+	partial class FormMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.cookBookDataSet1 = new CookBook.CookBookDataSet1();
+			this.recipeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.recipeTableAdapter = new CookBook.CookBookDataSet1TableAdapters.RecipeTableAdapter();
+			this.tableAdapterManager = new CookBook.CookBookDataSet1TableAdapters.TableAdapterManager();
+			this.labelRecipes = new System.Windows.Forms.Label();
+			this.listRecipes = new System.Windows.Forms.ListBox();
+			this.listIngredients = new System.Windows.Forms.ListBox();
+			this.labelIngredients = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.cookBookDataSet1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.recipeBindingSource)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// cookBookDataSet1
+			// 
+			this.cookBookDataSet1.DataSetName = "CookBookDataSet1";
+			this.cookBookDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+			// 
+			// recipeBindingSource
+			// 
+			this.recipeBindingSource.DataMember = "Recipe";
+			this.recipeBindingSource.DataSource = this.cookBookDataSet1;
+			// 
+			// recipeTableAdapter
+			// 
+			this.recipeTableAdapter.ClearBeforeFill = true;
+			// 
+			// tableAdapterManager
+			// 
+			this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+			this.tableAdapterManager.IngredientTableAdapter = null;
+			this.tableAdapterManager.RecipeIngredientTableAdapter = null;
+			this.tableAdapterManager.RecipeTableAdapter = this.recipeTableAdapter;
+			this.tableAdapterManager.UpdateOrder = CookBook.CookBookDataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+			// 
+			// labelRecipes
+			// 
+			this.labelRecipes.AutoSize = true;
+			this.labelRecipes.Location = new System.Drawing.Point(66, 34);
+			this.labelRecipes.Name = "labelRecipes";
+			this.labelRecipes.Size = new System.Drawing.Size(59, 17);
+			this.labelRecipes.TabIndex = 0;
+			this.labelRecipes.Text = "Recipes";
+			// 
+			// listRecipes
+			// 
+			this.listRecipes.FormattingEnabled = true;
+			this.listRecipes.ItemHeight = 16;
+			this.listRecipes.Location = new System.Drawing.Point(69, 66);
+			this.listRecipes.Name = "listRecipes";
+			this.listRecipes.Size = new System.Drawing.Size(204, 180);
+			this.listRecipes.TabIndex = 1;
+			this.listRecipes.SelectedIndexChanged += new System.EventHandler(this.listRecipes_SelectedIndexChanged);
+			// 
+			// listIngredients
+			// 
+			this.listIngredients.FormattingEnabled = true;
+			this.listIngredients.ItemHeight = 16;
+			this.listIngredients.Location = new System.Drawing.Point(402, 66);
+			this.listIngredients.Name = "listIngredients";
+			this.listIngredients.Size = new System.Drawing.Size(204, 180);
+			this.listIngredients.TabIndex = 3;
+			// 
+			// labelIngredients
+			// 
+			this.labelIngredients.AutoSize = true;
+			this.labelIngredients.Location = new System.Drawing.Point(399, 34);
+			this.labelIngredients.Name = "labelIngredients";
+			this.labelIngredients.Size = new System.Drawing.Size(78, 17);
+			this.labelIngredients.TabIndex = 2;
+			this.labelIngredients.Text = "Ingredients";
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(745, 402);
+			this.Controls.Add(this.listIngredients);
+			this.Controls.Add(this.labelIngredients);
+			this.Controls.Add(this.listRecipes);
+			this.Controls.Add(this.labelRecipes);
+			this.Name = "FormMain";
+			this.Text = "CookBook";
+			this.Load += new System.EventHandler(this.FormMain_Load);
+			((System.ComponentModel.ISupportInitialize)(this.cookBookDataSet1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.recipeBindingSource)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private CookBookDataSet1 cookBookDataSet1;
+		private System.Windows.Forms.BindingSource recipeBindingSource;
+		private CookBookDataSet1TableAdapters.RecipeTableAdapter recipeTableAdapter;
+		private CookBookDataSet1TableAdapters.TableAdapterManager tableAdapterManager;
+		private System.Windows.Forms.Label labelRecipes;
+		private System.Windows.Forms.ListBox listRecipes;
+		private System.Windows.Forms.ListBox listIngredients;
+		private System.Windows.Forms.Label labelIngredients;
+	}
+}
+
---
+++ CookBook/FormMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CookBook
+{
+	public partial class FormMain : Form
+	{
+		string connectionString;
+		SqlConnection connection;
+
+
+		public FormMain()
+		{
+			InitializeComponent();
+			connectionString = ConfigurationManager.ConnectionStrings["CookBook.Properties.Settings.CookBookConnectionString"].ConnectionString;
+		}
+
+		private void FormMain_Load(object sender, EventArgs e)
+		{
+			PopulateRecipes();
+		}
+
+		private void PopulateRecipes()
+		{
+			using(connection = new SqlConnection(connectionString))
+				using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Recipe",connection))
+			{
+				DataTable recipeTable = new DataTable();
+				adapter.Fill(recipeTable);
+				listRecipes.DisplayMember = "Name";
+				listRecipes.ValueMember = "Id";
+				listRecipes.DataSource = recipeTable;
+			}
+		}
+
+		private void PopulateIngredients()
+		{
+			string query = "SELECT a.Name FROM Ingredient a INNER JOIN RecipeIngredient b " +
+				"ON a.Id = b.IngredientId WHERE b.RecipeId = @RecipeId";
+			using(connection = new SqlConnection(connectionString))
+			using(SqlCommand command = new SqlCommand(query, connection))
+			using(SqlDataAdapter adapter = new SqlDataAdapter(command))
+			{
+				command.Parameters.AddWithValue("@RecipeId", listRecipes.SelectedValue);
+				DataTable ingredientTable = new DataTable();
+				adapter.Fill(ingredientTable);
+				listIngredients.DisplayMember = "Name";
+				listIngredients.ValueMember = "Id";
+				listIngredients.DataSource = ingredientTable;
+			}
+		}
+
+		private void listRecipes_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			//MessageBox.Show(listRecipes.SelectedValue.ToString());
+			PopulateIngredients();
+		}
+	}
+}
